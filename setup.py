fbabeddccd324f69a202e49ee800bfbdc4bae551
Benoit Pierre <benoit.pierre@gmail.com>
2017-03-30 21:35:27 +0200

    setup: add keywords

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
     ],
+    keywords = 'plover',
     setup_requires = [
         'setuptools-scm',
     ],
