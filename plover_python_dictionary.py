6958439bc9051ac0622568cb79657145d1005f20
Benoit Pierre <benoit.pierre@gmail.com>
2017-04-18 21:09:30 +0200

    update to Plover's latest API

--- plover_python_dictionary.py
+++ plover_python_dictionary.py
@@ -10,15 +10,33 @@ from plover import resource
 
 class PythonDictionary(StenoDictionary):
 
-    def __init__(self, mod):
+    def __init__(self):
         super(PythonDictionary, self).__init__()
-        self._mod = mod
-        self._longest_key = mod.LONGEST_KEY
-        if not hasattr(mod, 'reverse_lookup'):
-            mod.reverse_lookup = lambda value: ()
+        self._mod = None
+        self._lookup = None
+        self._reverse_lookup = None
 
-    def reverse_lookup(self, value):
-        return self._mod.reverse_lookup(value)
+    def _load(self, filename):
+        imp.acquire_lock()
+        try:
+            try:
+                mod = imp.load_source('', filename)
+            except Exception as e:
+                raise DictionaryLoaderException('Could not load dictionary: %s\n' % str(e))
+            else:
+                del sys.modules['']
+        finally:
+            imp.release_lock()
+        self._longest_key = getattr(mod, 'LONGEST_KEY', None)
+        if not isinstance(self._longest_key, int) or self._longest_key <= 0:
+            raise DictionaryLoaderException('Invalid dictionary: missing or invalid `LONGEST_KEY\' constant: %s\n' % str(longest_key))
+        self._lookup = getattr(mod, 'lookup', None)
+        if not isinstance(self._lookup, type(lambda x: x)):
+            raise DictionaryLoaderException('Invalid dictionary: missing or invalid `lookup\' function: %s\n' % str(lookup))
+        self._reverse_lookup = getattr(mod, 'reverse_lookup', lambda x: ())
+        if not isinstance(self._reverse_lookup, type(lambda x: x)):
+            raise DictionaryLoaderException('Invalid dictionary: invalid `reverse_lookup\' function: %s\n' % str(reverse_lookup))
+        self._mod = mod
 
     def __setitem__(self, key, value):
         raise NotImplementedError()
@@ -27,28 +45,7 @@ class PythonDictionary(StenoDictionary):
         raise NotImplementedError()
 
     def __getitem__(self, key):
-        return self._mod.lookup(key)
+        return self._lookup(key)
 
-
-def load_dictionary(name):
-    filename = resource.resource_filename(name)
-    imp.acquire_lock()
-    try:
-        try:
-            mod = imp.load_source('', filename)
-        except Exception as e:
-            raise DictionaryLoaderException('Could not load dictionary: %s\n' % str(e))
-        else:
-            del sys.modules['']
-    finally:
-        imp.release_lock()
-    longest_key = getattr(mod, 'LONGEST_KEY', None)
-    if not isinstance(longest_key, int) or longest_key <= 0:
-        raise DictionaryLoaderException('Invalid dictionary: missing or invalid `LONGEST_KEY\' constant: %s\n' % str(longest_key))
-    lookup = getattr(mod, 'lookup', None)
-    if not isinstance(lookup, type(lambda x: x)):
-        raise DictionaryLoaderException('Invalid dictionary: missing or invalid `lookup\' function: %s\n' % str(lookup))
-    reverse_lookup = getattr(mod, 'reverse_lookup', None)
-    if reverse_lookup is not None and not isinstance(reverse_lookup, type(lambda x: x)):
-        raise DictionaryLoaderException('Invalid dictionary: invalid `reverse_lookup\' function: %s\n' % str(reverse_lookup))
-    return PythonDictionary(mod)
+    def reverse_lookup(self, value):
+        return self._reverse_lookup(value)
