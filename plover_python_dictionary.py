1070fd3713c0c41221bd572a3833dae32077de0a
Benoit Pierre <benoit.pierre@gmail.com>
2017-04-21 19:26:29 +0200

    fix support for Plover's last API

--- plover_python_dictionary.py
+++ plover_python_dictionary.py
@@ -15,6 +15,7 @@ class PythonDictionary(StenoDictionary):
         self._mod = None
         self._lookup = None
         self._reverse_lookup = None
+        self.readonly = True
 
     def _load(self, filename):
         imp.acquire_lock()
@@ -46,6 +47,12 @@ class PythonDictionary(StenoDictionary):
 
     def __getitem__(self, key):
         return self._lookup(key)
+
+    def get(self, key, fallback=None):
+        try:
+            return self._lookup(key)
+        except KeyError:
+            return fallback
 
     def reverse_lookup(self, value):
         return self._reverse_lookup(value)
