d203839555ef8cedd8d868e958d9f70bfd536979
Benoit Pierre <benoit.pierre@gmail.com>
2017-04-26 21:14:35 +0200

    fix exception handling

--- plover_python_dictionary.py
+++ plover_python_dictionary.py
@@ -4,7 +4,6 @@ import imp
 import sys
 
 from plover.steno_dictionary import StenoDictionary
-from plover.exception import DictionaryLoaderException
 from plover import resource
 
 
@@ -20,23 +19,19 @@ class PythonDictionary(StenoDictionary):
     def _load(self, filename):
         imp.acquire_lock()
         try:
-            try:
-                mod = imp.load_source('', filename)
-            except Exception as e:
-                raise DictionaryLoaderException('Could not load dictionary: %s\n' % str(e))
-            else:
-                del sys.modules['']
+            mod = imp.load_source('', filename)
+            del sys.modules['']
         finally:
             imp.release_lock()
         self._longest_key = getattr(mod, 'LONGEST_KEY', None)
         if not isinstance(self._longest_key, int) or self._longest_key <= 0:
-            raise DictionaryLoaderException('Invalid dictionary: missing or invalid `LONGEST_KEY\' constant: %s\n' % str(longest_key))
+            raise ValueError('missing or invalid `LONGEST_KEY\' constant: %s\n' % str(longest_key))
         self._lookup = getattr(mod, 'lookup', None)
         if not isinstance(self._lookup, type(lambda x: x)):
-            raise DictionaryLoaderException('Invalid dictionary: missing or invalid `lookup\' function: %s\n' % str(lookup))
+            raise ValueError('missing or invalid `lookup\' function: %s\n' % str(lookup))
         self._reverse_lookup = getattr(mod, 'reverse_lookup', lambda x: ())
         if not isinstance(self._reverse_lookup, type(lambda x: x)):
-            raise DictionaryLoaderException('Invalid dictionary: invalid `reverse_lookup\' function: %s\n' % str(reverse_lookup))
+            raise ValueError('invalid `reverse_lookup\' function: %s\n' % str(reverse_lookup))
         self._mod = mod
 
     def __setitem__(self, key, value):
