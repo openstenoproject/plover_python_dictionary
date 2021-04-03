74e609a410dc65baa64752b39ddd2eb31f354d5c
Benoit Pierre <benoit.pierre@gmail.com>
2021-04-04 00:31:13 +0200

    rework tests
    - do not use `unittest` module
    - make use of `plover_build_utils.testing`

--- test/test_blackbox.py
+++ test/test_blackbox.py
@@ -1,84 +1,53 @@
-
 import os
-import unittest
 
 from plover import log, system
 from plover.config import DEFAULT_SYSTEM_NAME
 from plover.dictionary.base import load_dictionary
 from plover.formatting import Formatter
 from plover.registry import registry
-from plover.steno import normalize_steno, Stroke
+from plover.steno import normalize_steno
 from plover.steno_dictionary import StenoDictionary
 from plover.translation import Translator
+
+from plover_build_utils.testing import CaptureOutput, steno_to_stroke
+
+import pytest
 
 
 log.set_level(log.DEBUG)
 
 
-class CaptureOutput(object):
-
-    def __init__(self):
-        self.instructions = []
-        self.text = u''
-
-    def send_backspaces(self, n):
-        assert n <= len(self.text)
-        self.text = self.text[:-n]
-        self.instructions.append(('b', n))
-
-    def send_string(self, s):
-        self.text += s
-        self.instructions.append(('s', s))
-
-    def send_key_combination(self, c):
-        self.instructions.append(('c', c))
-
-    def send_engine_command(self, c):
-        self.instructions.append(('e', c))
-
-
-def steno_to_stroke(steno):
-    stroke = Stroke(())
-    stroke.rtfcre = steno
-    return stroke
-
-
-class BlackboxTest(unittest.TestCase):
-
-    @classmethod
-    def setUpClass(cls):
-        registry.update()
-        system.setup(DEFAULT_SYSTEM_NAME)
-
-    def setUp(self):
-        self.output = CaptureOutput()
-        self.formatter = Formatter()
-        self.formatter.set_output(self.output)
-        self.translator = Translator()
-        self.translator.add_listener(self.formatter.format)
-        self.dictionary = self.translator.get_dictionary()
-        dictionary = StenoDictionary()
-        dictionary.save = lambda: None
-        self.dictionary.set_dicts([dictionary])
-
-    def test_basic(self):
-        d = load_dictionary(os.path.join(os.path.dirname(__file__), 'show_stroke.py'))
-        self.assertEqual(d.readonly, True)
-        self.assertEqual(d.longest_key, 2)
-        with self.assertRaises(KeyError):
-            d[('STR',)]
-        self.assertEqual(d.get(('STR',)), None)
-        self.assertEqual(d[('STR*', 'STR')], 'STR')
-        self.assertEqual(d.get(('STR*', 'STR')), 'STR')
-        self.assertEqual(d.reverse_lookup('STR'), [])
-        self.dictionary.set_dicts([d] + self.dictionary.dicts)
-        self.dictionary.set(normalize_steno('STR'), u'center')
-        for steno in (
-            'STR',
-            'STR*',
-            'STR',
-            'STR',
-        ):
-            stroke = steno_to_stroke(steno)
-            self.translator.translate(stroke)
-        self.assertEqual(self.output.text, u' center STR center')
+def test_python_dictionary():
+    # Setup.
+    registry.update()
+    system.setup(DEFAULT_SYSTEM_NAME)
+    output = CaptureOutput()
+    formatter = Formatter()
+    formatter.set_output(output)
+    translator = Translator()
+    translator.add_listener(formatter.format)
+    dictionary = translator.get_dictionary()
+    empty_dict = StenoDictionary()
+    empty_dict.save = lambda: None
+    dictionary.set_dicts([empty_dict])
+    # Test.
+    d = load_dictionary(os.path.join(os.path.dirname(__file__), 'show_stroke.py'))
+    assert d.readonly is True
+    assert d.longest_key == 2
+    with pytest.raises(KeyError):
+        d[('STR',)]
+    assert d.get(('STR',)) is None
+    assert d[('STR*', 'STR')] == 'STR'
+    assert d.get(('STR*', 'STR')) == 'STR'
+    assert d.reverse_lookup('STR') == []
+    dictionary.set_dicts([d] + dictionary.dicts)
+    dictionary.set(normalize_steno('STR'), 'center')
+    for steno in (
+        'STR',
+        'STR*',
+        'STR',
+        'STR',
+    ):
+        stroke = steno_to_stroke(steno)
+        translator.translate(stroke)
+    assert output.text == ' center STR center'
